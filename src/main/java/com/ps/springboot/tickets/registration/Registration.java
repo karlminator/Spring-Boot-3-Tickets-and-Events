4c57eaf28f5d9b49248caacd8fc034f0e317a2bb
karlminator <kf.holmsten@gmail.com>
2025-02-18 16:47:55 +0100

    added some exception handling and validation.

--- src/main/java/com/ps/springboot/tickets/registration/Registration.java
+++ src/main/java/com/ps/springboot/tickets/registration/Registration.java
@@ -1,18 +1,11 @@
 package com.ps.springboot.tickets.registration;
 
-//import jakarta.validation.constraints.NotBlank;
-//import jakarta.validation.constraints.NotNull;
-
-//public record Registration(
-//        Integer id,
-//        @NotNull(message = "Product id is required") Integer productId,
-//        String ticketCode,
-//        @NotBlank(message = "Attendee name is required") String attendeeName) {
-//}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record Registration(
         Integer id,
-        Integer productId,
+        @NotNull(message = "Product id is required") Integer productId,
         String ticketCode,
-        String attendeeName) {
+        @NotBlank(message = "Attendee name is required") String attendeeName) {
 }
